588182f4fd3c06ed89b4e253dd44155a02072976
karth <karthiknaveen22022002@gmail.com>
2022-04-01 19:47:09 +0530

    phase 1 contactInfo

---
+++ src/DATA/TeamInfo.jsx
@@ -0,0 +1,194 @@
+export const Electronics = [
+  {
+    id: 1,
+
+    img: "https://i.ibb.co/ctDzJZZ/img1.png",
+    name: "username1",
+    desc: "some randon text some randon textsome randon textsome randon textsome randon textsome randon textsome randon text",
+    linkedin: "url",
+    mail: "url",
+    insta: "url",
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/ctDzJZZ/img1.png",
+    // img: "Electwo",
+    name: "username2",
+    desc: "some randon text some randon textsome randon textsome randon textsome randon textsome randon textsome randon text",
+    linkedin: "url",
+    mail: "url",
+    insta: "url",
+  },
+  {
+    id: 3,
+    img: "https://i.ibb.co/ctDzJZZ/img1.png",
+    // img: "Electhree",
+    name: "username3",
+    desc: "some randon text some randon textsome randon textsome randon textsome randon textsome randon textsome randon text",
+    linkedin: "url",
+    mail: "url",
+    insta: "url",
+  },
+  {
+    id: 4,
+    img: "https://i.ibb.co/ctDzJZZ/img1.png",
+    // img: "Elecfour",
+    name: "username4",
+    desc: "some randon text some randon textsome randon textsome randon textsome randon textsome randon textsome randon text",
+    linkedin: "url",
+    mail: "url",
+    insta: "url",
+  },
+  {
+    id: 5,
+    img: "https://i.ibb.co/ctDzJZZ/img1.png",
+    // img: "Elecfive",
+    name: "username1",
+    desc: "some randon text some randon textsome randon textsome randon textsome randon textsome randon textsome randon text",
+    linkedin: "url",
+    mail: "url",
+    insta:"url"
+  },
+  {
+    id: 6,
+    img: "https://i.ibb.co/ctDzJZZ/img1.png",
+    // img: "Elecsix",
+    name: "username2",
+    desc: "some randon text some randon textsome randon textsome randon textsome randon textsome randon textsome randon text",
+    linkedin: "url",
+    mail: "url",
+    insta:"url"
+  },
+];
+
+
+export const Programming = [
+  {
+    id: 1,
+    img: "https://i.ibb.co/ctDzJZZ/img1.png",
+    // img: "Proone",
+    name: "username1",
+    desc: "some randon text some randon textsome randon textsome randon textsome randon textsome randon textsome randon text",
+    linkedin: "url",
+    mail: "url",
+    insta:"url"
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/ctDzJZZ/img1.png",
+    // img: "Protwo",
+    name: "username2",
+    desc: "some randon text some randon textsome randon textsome randon textsome randon textsome randon textsome randon text",
+    linkedin: "url",
+    mail: "url",
+    insta:"url"
+  },
+  {
+    id: 3,
+    img: "https://i.ibb.co/ctDzJZZ/img1.png",
+    // img: "Prothree",
+    name: "username3",
+    desc: "some randon text some randon textsome randon textsome randon textsome randon textsome randon textsome randon text",
+    linkedin: "url",
+    mail: "url",
+    insta:"url"
+  },
+  {
+    id: 4,
+    img: "https://i.ibb.co/ctDzJZZ/img1.png",
+    // img: "Profour",
+    name: "username4",
+    desc: "some randon text some randon textsome randon textsome randon textsome randon textsome randon textsome randon text",
+    linkedin: "url",
+    mail: "url",
+    insta:"url"
+  },
+  {
+    id: 5,
+    img: "https://i.ibb.co/ctDzJZZ/img1.png",
+    // img: "Profive",
+    name: "username1",
+    desc: "some randon text some randon textsome randon textsome randon textsome randon textsome randon textsome randon text",
+    linkedin: "url",
+    mail: "url",
+    insta:"url"
+  },
+  {
+    id: 6,
+    img: "https://i.ibb.co/ctDzJZZ/img1.png",
+    // img: "Prosix",
+    name: "username2",
+    desc: "some randon text some randon textsome randon textsome randon textsome randon textsome randon textsome randon text",
+    linkedin: "url",
+    mail: "url",
+    insta:"url"
+  },
+ 
+];
+
+
+
+
+
+export const Design = [
+  {
+    id: 1,
+    img: "https://i.ibb.co/ctDzJZZ/img1.png",
+    // img: "Desone",
+    name: "username1",
+    desc: "some randon text some randon textsome randon textsome randon textsome randon textsome randon textsome randon text",
+    linkedin: "url",
+    mail: "url",
+    insta:"url"
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/ctDzJZZ/img1.png",
+    // img: "Destwo",
+    name: "username2",
+    desc: "some randon text some randon textsome randon textsome randon textsome randon textsome randon textsome randon text",
+    linkedin: "url",
+    mail: "url",
+    insta:"url"
+  },
+  {
+    id: 3,
+    img: "https://i.ibb.co/ctDzJZZ/img1.png",
+    // img: "Desthree",
+    name: "username3",
+    desc: "some randon text some randon textsome randon textsome randon textsome randon textsome randon textsome randon text",
+    linkedin: "url",
+    mail: "url",
+    insta:"url"
+  },
+  {
+    id: 4,
+    img: "https://i.ibb.co/ctDzJZZ/img1.png",
+    // img: "Desfour",
+    name: "username4",
+    desc: "some randon text some randon textsome randon textsome randon textsome randon textsome randon textsome randon text",
+    linkedin: "url",
+    mail: "url",
+    insta:"url"
+  },
+  {
+    id: 5,
+    img: "https://i.ibb.co/ctDzJZZ/img1.png",
+    // img: "Desfive",
+    name: "username1",
+    desc: "some randon text some randon textsome randon textsome randon textsome randon textsome randon textsome randon text",
+    linkedin: "url",
+    mail: "url",
+    insta:"url"
+  },
+  {
+    id: 6,
+    img: "https://i.ibb.co/ctDzJZZ/img1.png",
+    // img: "Dessix",
+    name: "username2",
+    desc: "some randon text some randon textsome randon textsome randon textsome randon textsome randon textsome randon text",
+    linkedin: "url",
+    mail: "url",
+    insta:"url"
+  },
+];
--- src/components/TeamInfo.jsx
+++ src/components/TeamInfo.jsx
@@ -1,9 +1,188 @@
-import React from 'react'
+import React from "react";
+
+import styled from "styled-components";
+
+import { Electronics, Programming, Design } from "../DATA/TeamInfo";
+
+import { BsLinkedin, BsInstagram } from "react-icons/bs";
+
+import { IoMail } from "react-icons/io5";
+
+const SocialMediaContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  color: black;
+`;
+
+const ImgContainer = styled.div`
+  display: flex;
+
+  justify-content: center;
+`;
+
+const CardImage = styled.img`
+  margin-top: 20px;
+  margin-bottom: 15px;
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  background-color: white;
+`;
+
+const CardContainer = styled.div`
+  margin: 10px;
+  display: grid;
+  grid-gap: 25px;
+  grid-auto-rows: 400px;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+`;
+
+const NameContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const DescriptionContainer = styled.div`
+  margin-left: 15px;
+  margin-right: 15px;
+  text-align: justify;
+`;
+
+const Card = styled.div`
+  display: flex;
+  /* justify-content: center; */
+  flex-direction: column;
+  background-color: ${(props) => props.color};
+  -webkit-box-shadow: 0px 0px 12px 1px #444444;
+  box-shadow: 0px 0px 12px 1px #444444;
+  border-radius: 5px;
+
+  &:hover {
+    background-color: #2e2b2b;
+    ${CardImage} {
+      background-color: ${(props) => props.color};
+    }
+
+    ${DescriptionContainer} {
+      color: ${(props) => props.color};
+    }
+
+    ${DescriptionContainer} {
+      color: ${(props) => props.color};
+    }
+
+    ${NameContainer} {
+      color: ${(props) => props.color};
+    }
+
+    ${SocialMediaContainer} {
+      color: ${(props) => props.color};
+    }
+  }
+`;
 
 const TeamInfo = () => {
-		return (
-				<div>TeamInfo</div>
-		)
-}
+  const beautifulColors = [
+    "#78ffd0",
+    "#f8abff",
+    "#dbff70",
+    "#fc97b1",
+    "#a696ff",
+  ];
 
-export default TeamInfo
+  return (
+    <div>
+      <h1>Worked on electronics</h1>
+      <CardContainer>
+        {Electronics.map((item, id) => {
+          return (
+            <Card color={beautifulColors[id % 5]}>
+              <ImgContainer>
+                <CardImage src={item.img}></CardImage>
+              </ImgContainer>
+
+              <NameContainer>
+                <h1>{item.name}</h1>
+              </NameContainer>
+              <DescriptionContainer>{item.desc}</DescriptionContainer>
+
+              <SocialMediaContainer>
+                <a href={item.linkedin}>
+                  <BsLinkedin size="30px" />
+                </a>
+                <a href={item.linkedin}>
+                  <BsInstagram size="30px" />
+                </a>
+                <a href={item.linkedin}>
+                  <IoMail size="30px" />
+                </a>
+              </SocialMediaContainer>
+            </Card>
+          );
+        })}
+      </CardContainer>
+
+      <h1>Worked on Programming</h1>
+      <CardContainer>
+        {Programming.map((item, id) => {
+          return (
+            <Card color={beautifulColors[id % 5]}>
+              <ImgContainer>
+                <CardImage src={item.img}></CardImage>
+              </ImgContainer>
+
+              <NameContainer>
+                <h1>{item.name}</h1>
+              </NameContainer>
+              <DescriptionContainer>{item.desc}</DescriptionContainer>
+
+              <SocialMediaContainer>
+                <a href={item.linkedin}>
+                  <BsLinkedin size="30px" />
+                </a>
+                <a href={item.linkedin}>
+                  <BsInstagram size="30px" />
+                </a>
+                <a href={item.linkedin}>
+                  <IoMail size="30px" />
+                </a>
+              </SocialMediaContainer>
+            </Card>
+          );
+        })}
+      </CardContainer>
+
+      <h1>Worked on Design</h1>
+      <CardContainer>
+        {Design.map((item, id) => {
+          return (
+            <Card color={beautifulColors[id % 5]}>
+              <ImgContainer>
+                <CardImage src={item.img}></CardImage>
+              </ImgContainer>
+
+              <NameContainer>
+                <h1>{item.name}</h1>
+              </NameContainer>
+              <DescriptionContainer>{item.desc}</DescriptionContainer>
+
+              <SocialMediaContainer>
+                <a href={item.linkedin}>
+                  <BsLinkedin size="30px" />
+                </a>
+                <a href={item.linkedin}>
+                  <BsInstagram size="30px" />
+                </a>
+                <a href={item.linkedin}>
+                  <IoMail size="30px" />
+                </a>
+              </SocialMediaContainer>
+            </Card>
+          );
+        })}
+      </CardContainer>
+    </div>
+  );
+};
+
+export default TeamInfo;
